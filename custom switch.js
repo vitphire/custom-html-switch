c7a14a05714d8c918befd3fc0f2b23d85732a0a9
vitrack <61618352+vitrack@users.noreply.github.com>
2020-11-03 15:45:32 +0100

    Add files via upload

---
+++ custom switch.js
@@ -0,0 +1,29 @@
+// create the CustomSwitch class
+class CustomSwitch extends HTMLLabelElement {
+    constructor() {
+        super();
+    }
+    //what happens when a switch is loaded in
+    connectedCallback() {
+        //make it have the class "switch"
+        this.classList.add("switch");
+        //create checkbox and add it
+        var chckbox = document.createElement("input");
+        chckbox.type="checkbox";
+        if (this.getAttribute("checked")=="") {
+            chckbox.checked=true
+            this.removeAttribute("checked")
+        }
+        this.appendChild(chckbox);
+        //create the inner and outer parts and add them
+        var outer = document.createElement("div");
+        outer.classList.add("outer");
+        var inner = document.createElement("div");
+        inner.classList.add("inner");
+        outer.appendChild(inner);
+        this.appendChild(outer);
+    }
+}
+
+//add the class to the elements
+customElements.define('custom-switch', CustomSwitch, {extends:'label'});
