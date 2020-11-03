8614450b9672bb187ebcfb55ce20efaa86f27a48
vitrack <61618352+vitrack@users.noreply.github.com>
2020-11-03 18:47:47 +0100

    Update custom switch.js
    Made the switch checked when the checkbox inside is checked

--- custom switch.js
+++ custom switch.js
@@ -7,14 +7,22 @@ class CustomSwitch extends HTMLLabelElement {
     connectedCallback() {
         //make it have the class "switch"
         this.classList.add("switch");
+
         //create checkbox and add it
         var chckbox = document.createElement("input");
         chckbox.type="checkbox";
         if (this.getAttribute("checked")=="") {
-            chckbox.checked=true
-            this.removeAttribute("checked")
+            chckbox.checked=true;
+            this.removeAttribute("checked");
         }
         this.appendChild(chckbox);
+        //make the switch checked if the checkbox is
+        this.checked=this.getElementsByTagName("input")[0].checked
+        chckbox.addEventListener("input", chckboxClicked)
+        function chckboxClicked() {
+            this.parentElement.checked = this.checked;
+        }
+
         //create the inner and outer parts and add them
         var outer = document.createElement("div");
         outer.classList.add("outer");
